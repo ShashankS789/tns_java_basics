ea3a5a819694754a8bfd1fcca26778c3eeea3211
ShashankS789 <shashanks1190@gmail.com>
2025-08-18 15:01:47 +0530

    Add files via upload

---
+++ core_basics.java
@@ -0,0 +1,14 @@
+package com.tnsif.packagetwo;
+
+import com.tnsif.packageone.Base;
+
+public class ExecutorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Base  b = new Base();
+ b.varpublic=12;
+ b.methodPublic();
+	}
+
+}
